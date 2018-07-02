c10e25bc075099b6f266bfbf8384134327f6bf4c
Russell Heimlich <kingkool68@gmail.com>
2018-07-01 23:37:03 -0400

    Add a way to extend Twig using filters and actions

--- class-sprig.php
+++ class-sprig.php
@@ -1,9 +1,5 @@
 <?php
 
-// namespace Sprig;
-/**
- * [Sprig description]
- */
 class Sprig {
 
 	static $twig;
@@ -16,13 +12,13 @@ class Sprig {
 		if ( null === $instance ) {
 			$instance = new static();
 			$instance->setup_twig();
+			$instance->setup_actions();
+			$instance->setup_filters();
 		}
 		return $instance;
 	}
 
-	public function  __construct() {
-		// $this->setup_twig();
-	}
+	public function  __construct() {}
 
 	public function setup_twig() {
 		$open_basedir = ini_get( 'open_basedir' );
@@ -44,8 +40,54 @@ class Sprig {
 		self::$twig = $twig;
 	}
 
+	public function setup_actions() {
+		add_action( 'init', [ $this, 'action_init' ] );
+	}
+
+	public function action_init() {
+		// Add custom filters to Twig
+		// See https://twig.symfony.com/doc/2.x/advanced.html#filters
+		$twig_filters = apply_filters( 'sprig/twig/filters', array() );
+		foreach ( $twig_filters as $name => $filter_callback ) {
+			if ( is_callable( $filter_callback ) ) {
+				self::$twig->addFilter( new Twig_SimpleFilter( $name, $filter_callback ) );
+			}
+		}
+
+		// Add custom filters to Twig
+		// See https://twig.symfony.com/doc/2.x/advanced.html#functions
+		$twig_functions = apply_filters( 'sprig/twig/functions', array() );
+		foreach ( $twig_functions as $name => $function_callback ) {
+			if ( is_callable( $function_callback ) ) {
+				self::$twig->addFunction( new Twig_SimpleFunction( $name, $function_callback ) );
+			}
+		}
+
+		// Modify Twig itself
+		// See https://twig.symfony.com/doc/2.x/advanced.html#extending-twig
+		self::$twig = apply_filters( 'sprig/twig', self::$twig );
+	}
+
+	public function setup_filters() {
+		add_filter( 'sprig/twig/filters', [ $this, 'filter_sprig_twig_default_filters' ] );
+		add_filter( 'sprig/twig/functions', [ $this, 'filter_sprig_twig_default_functions' ] );
+	}
+
+	public function filter_sprig_twig_default_filters( $filters = array() ) {
+		$filters['esc_attr'] = 'esc_attr';
+		return $filters;
+	}
+
+	public function filter_sprig_twig_default_functions( $functions = array() ) {
+		$functions['checked'] = 'checked';
+		$functions['selected'] = 'selected',
+		$functins['disabled'] = 'disabled'
+		return $functions;
+	}
+
 	public function get_template_locations() {
-		$theme_locs = array();
+		$theme_locations = array();
+		$theme_dirs = apply_filters( 'sprig/theme_dirs', array( 'views', 'twig' ) );
 		$roots = array( get_stylesheet_directory(), get_template_directory() );
 		$roots = array_map( 'realpath', $roots );
 		$roots = array_unique( $roots );
@@ -53,23 +95,22 @@ class Sprig {
 			if ( ! is_dir( $root ) ) {
 				continue;
 			}
-			$theme_locs[] = $root;
+			$theme_locations[] = $root;
 			$root = trailingslashit( $root );
 			foreach ( $theme_dirs as $dirname ) {
-				$tloc = realpath( $root . $dirname );
-				if ( is_dir( $tloc ) ) {
-					$theme_locs[] = $tloc;
+				$theme_location = realpath( $root . $dirname );
+				if ( is_dir( $theme_location ) ) {
+					$theme_locations[] = $theme_location;
 				}
 			}
 		}
-		return $theme_locs;
+		return $theme_locations;
 	}
 
 	public static function render( $filenames, $data = array() ) {
 		if ( ! is_array( $filenames ) ) {
 			$filenames = array( $filenames );
 		}
-		// @TODO Figure out the right template to use based on parent theme, child theme, filtering etc.
 		$output = self::$twig->render( $filenames[0], $data );
 		return $output;
 	}
--- sprig.php
+++ sprig.php
@@ -8,9 +8,9 @@ Version: 0.0.1
 Author URI: https://russellheimlich.com
 */
 
-// we look for Composer files first in the plugins dir.
-// then in the wp-content dir (site install).
-// and finally in the current themes directories.
+// We look for Composer files first in the plugins dir.
+// Then in the wp-content dir (site install).
+// And finally in the current themes directories.
 if ( file_exists( $composer_autoload = __DIR__ . '/vendor/autoload.php' ) /* check in self */
 	|| file_exists( $composer_autoload = WP_CONTENT_DIR . '/vendor/autoload.php' ) /* check in wp-content */
 	|| file_exists( $composer_autoload = plugin_dir_path( __FILE__ ) . 'vendor/autoload.php' ) /* check in plugin directory */
