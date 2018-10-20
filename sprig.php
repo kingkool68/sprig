fdac4d2e7dee6d4697dfce46f20b56238fe166f0
Russell Heimlich <kingkool68@gmail.com>
2018-10-20 00:05:32 -0400

    Add GitHub Updater plugin support
    See https://github.com/afragen/github-updater Though this probably won't work as Composer needs to be run to download Twig and its dependencies.

--- sprig.php
+++ sprig.php
@@ -3,6 +3,7 @@
 Plugin Name: Sprig
 Description: Minimal Twig template engine setup.
 Plugin URI: https://github.com/kingkool68/sprig
+GitHub Plugin URI: kingkool68/sprig
 Author: Russell Heimlich
 Version: 0.0.1
 Author URI: https://russellheimlich.com
